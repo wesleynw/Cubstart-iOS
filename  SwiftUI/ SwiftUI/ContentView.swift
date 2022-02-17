d91abab267bdf8b7d8dc8ff0af1c5cd8dd7844ef
Tony <tshong@berkeley.edu>
2022-02-17 03:30:35 -0800

    skeleton code

---
+++  SwiftUI/ SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//   SwiftUI
+//
+//  Created by Tony Hong on 2/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++  SwiftUI/ SwiftUI/_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  _SwiftUIApp.swift
+//   SwiftUI
+//
+//  Created by Tony Hong on 2/17/22.
+//
+
+import SwiftUI
+
+@main
+struct _SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
